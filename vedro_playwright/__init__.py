8445d82bc7a0d28fbadd5cc1f1a5c5006db9aab3
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-07-20 15:14:03 +0300

    bump version → 1.1.0

--- vedro_playwright/__init__.py
+++ vedro_playwright/__init__.py
@@ -9,7 +9,7 @@ from ._browsers import (
 )
 from ._vedro_playwright import Playwright, PlaywrightPlugin, opened_browser, opened_browser_page
 
-__version__ = "0.2.0"
+__version__ = "1.1.0"
 __all__ = ("Playwright", "PlaywrightPlugin", "BrowserEngine",
            "opened_browser", "opened_browser_page",
            "opened_chromium", "opened_chromium_page",
